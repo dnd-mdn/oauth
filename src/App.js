8ff7867a80d22736211c1c61907c962e8499fb40
Ben Soicher <ben.soicher@protonmail.com>
2024-06-07 09:59:47 -0400

    testing with auth0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
-import { useContext } from 'react';
-import UserContext from './User';
-
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  const user = useContext(UserContext);
+  const { isLoading, isAuthenticated, error, user, getAccessTokenWithPopup, logout } = useAuth0();
+
+  let button = null
+  if (isAuthenticated) {
+    button = (
+      <div>
+        Hello {user.name}{' '}
+        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+          Log out
+        </button>
+      </div>
+    );
+  } else {
+    button = <button onClick={() => getAccessTokenWithPopup()}>Log in</button>;
+  }
 
   return (
     <div className="App">
@@ -19,7 +31,7 @@ function App() {
 
       <Container>
         <h1 class="h2">GitHub oauth test</h1>
-        <p>{user.name}</p>
+        <p>{button}</p>
       </Container>
     </div>
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+
+root.render(
+    <React.StrictMode>
+        <Auth0Provider
+            domain="https://github.com/login/oauth"
+            clientId="Iv1.5dceb0507b750647"
+            authorizationParams={{
+                redirect_uri: "https://dnd-mdn.github.io/oauth/pop.html",
+                scope: "read:user",
+                allow_signup: "false"
+            }}
+            cacheLocation="localstorage"
+        >
+            <App />
+        </Auth0Provider>
+    </React.StrictMode>
+);

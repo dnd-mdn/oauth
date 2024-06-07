59a246b028d877aca4599066b5015884381e4f58
Ben Soicher <ben.soicher@protonmail.com>
2024-06-07 10:34:59 -0400

    fix redirect uri

--- src/Auth.js
+++ src/Auth.js
@@ -12,11 +12,10 @@ const AuthProvider = ({ children }) => {
         <Auth0Provider
             domain="https://github.com/login/oauth"
             clientId="Iv1.5dceb0507b750647"
-            redirectUri="https://dnd-mdn.github.io/oauth/pop.html"
+            redirectUri={window.location.origin}
             onRedirectCallback={onRedirectCallback}
             cacheLocation="localstorage"
             authorizationParams={{
-                redirect_uri: window.location.origin,
                 scope: "read:user",
                 allow_signup: "false"
             }}
